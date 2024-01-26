a287db05f7c465338a6a59f7d17f3929dab192b1
AndreiLapotko <shudyx@gmail.com>
2024-01-26 19:45:19 +1000

    HomeWork of Seminar10

---
+++ Seminar10/main.py
@@ -0,0 +1,11 @@
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.loc[data['whoAmI'] == 'human', 'human'] = 1
+data.loc[data['whoAmI'] == 'human', 'robot'] = 0
+data.loc[data['whoAmI'] == 'robot', 'human'] = 0
+data.loc[data['whoAmI'] == 'robot', 'robot'] = 1
+data[['human', 'robot']] = data[['human', 'robot']].astype(int)
+data.head(30)
